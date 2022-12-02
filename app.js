ea02de80398dee01cbb2730a9f67c7ed54ab9b76
thegu5 <58223632+thegu5@users.noreply.github.com>
2022-12-02 16:55:34 -0500

    add 5 second cache timer (part 2 electric boogaloo)

--- app.js
+++ app.js
@@ -18,22 +18,32 @@ async function getSheetData() {
   return response.data.values;
 }
 
-// ========== ROUTES ==========
+let timestamp = -1;
+let cache = null;
 
+
+// ========== ROUTES ==========
+app.get('/', (req, res) => {
+  res.send('This is the StuyPulse link shortener server!');
+});
 app.get('/*', (req, res) => {
   // alias refers to the "alias" in "link.stuypulse.com/alias"
   const alias = req.path.slice(1);
-
-  getSheetData().then((data) => {
-    for (const [linkAlias, destinationLink] of data) {
-      if (alias === linkAlias) {
-        res.redirect(destinationLink);
-        return;
-      }
+  if(Date.now() > timestamp + 5000) {
+    // console.log('Updating cache... ' + Date.now() + 'and the last cache time: ' + timestamp);
+    timestamp = Date.now();
+    getSheetData().then(data => {
+      cache = data;
+      // console.log('Cache updated!');
+    });
+  }
+  for (const [linkAlias, destinationLink] of cache) {
+    if (alias === linkAlias) {
+      res.redirect(destinationLink);
+      return;
     }
-
-    res.send('StuyPulse Link Shortener');
-  });
+  }
+    res.send('This shortlink does not exist. Check your spelling?');
 });
 
 app.listen(process.env.PORT, () => {
